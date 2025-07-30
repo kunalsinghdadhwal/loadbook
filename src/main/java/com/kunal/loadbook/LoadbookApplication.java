3223f69f2b21b8bb69d5bf4aea3ac1edcb9c5b42
Hug_Bunter <kunalsinghdadhwal@gmail.com>
2025-07-31 00:44:50 +0530

    init

---
+++ src/main/java/com/kunal/loadbook/LoadbookApplication.java
@@ -0,0 +1,13 @@
+package com.kunal.loadbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kunal/loadbook/LoadbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.loadbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
